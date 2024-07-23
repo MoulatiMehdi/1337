8d391727793e6560661598403e871eeaf74f7522
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-07-23 21:09:39 +0100

    add validation functions

---
+++ in_progress/BSQ/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 22:28:53 by mmoulati          #+#    #+#             */
+/*   Updated: 2024/07/17 22:29:29 by mmoulati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H
+
+#include "ft_utils.h"
+
+int get_height(char *str);
+char *get_sets(char *str);
+int is_valid_data(char **data);
+
+#endif
