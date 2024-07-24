251e6a5a189ef2ccbf9799920ef021a895e32ac7
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-07-24 20:47:26 +0100

    add main.c file to test create_grid function

--- in_progress/BSQ/includes/ft.h
+++ in_progress/BSQ/includes/ft.h
@@ -31,6 +31,8 @@ int	**create_matrix(int rows, int cols);
 void	init_matrix(int **matrx, struct s_grid map);
 char	**create_strs(int rows, int cols);
 
+struct s_grid create_grid(char *str);
+
 
 int is_valid_data(char **data);
 char *get_sets(char *str);
--- in_progress/BSQ/tests/main.c
+++ in_progress/BSQ/tests/main.c
@@ -5,9 +5,17 @@
 void fail(char *desc, char **expected, char **result);
 void pass(char *desc);
 int compate_maps(char **map1, char **map2);
-
+void print_grid(struct s_grid grid);
 
 int main(void)
 {
+    struct s_grid grid;
+
+    grid = create_grid("2.ox\n.o\n.o\n");
+
+   if(grid.data) 
+    print_grid(grid);
+    else
+    printf("(null)");
 }
 
