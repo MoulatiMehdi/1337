0aff7d04d66972f0f243b4a49b98648b4f786934
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-07-25 02:58:17 +0100

    read files using ft_realloc instead of calculating the content in read_file function

--- in_progress/BSQ/srcs/read.c
+++ in_progress/BSQ/srcs/read.c
@@ -12,75 +12,20 @@
 
 #include "ft.h"
 
-long long file_size(char *filename)
-{
-	int fd;
-	char buffer[BUFF_SIZE];
-	long long size;
-	int bytes;
-
-	fd = open(filename, O_RDONLY);
-	size = 0;
-	bytes = 1;
-	if (fd < 0)
-		return (-1);
-	while (1)
-	{
-		bytes = read(fd, buffer, BUFF_SIZE);
-		if (bytes <= 0)
-			break;
-		size += bytes;
-	}
-	if (bytes < 0)
-	{
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-	return (size);
-}
-
-char *read_file(char *filename)
-{
-	int fd;
-	long long bytes;
-	char *str;
-	int size;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	size = file_size(filename);
-	if (size <= 0)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (size + 1));
-	bytes = read(fd, str, size);
-	if (bytes < 0)
-	{
-		close(fd);
-		free(str);
-		return (str = NULL);
-	}
-	close(fd);
-	str[size] = '\0';
-	return (str);
-}
-
-char *read_input()
+char *get_text(int fd)
 {
 	char tmp_buff[BUFF_SIZE];
-	char *buffer;
 	int bytes;
+	char *buffer;
 	int len;
 	int j;
 
-	buffer = (char *)malloc(sizeof(char) * BUFF_SIZE);
-	if (!buffer)
-		return NULL;
+	j = 0;
 	len = 0;
+	buffer = NULL;
 	while (1)
 	{
-		bytes = read(0, tmp_buff, BUFF_SIZE);
+		bytes = read(fd, tmp_buff, BUFF_SIZE);
 		if (bytes < 1)
 			break;
 		buffer = ft_realloc(buffer, sizeof(char) * (len + bytes + 1));
@@ -94,4 +39,22 @@ char *read_input()
 	}
 	buffer[len] = '\0';
 	return buffer;
+}
+
+char *read_file(char *filename)
+{
+	int fd;
+	char *str;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	str = get_text(fd);
+	close(fd);
+	return (str);
+}
+
+char *read_input()
+{
+	return get_text(0);
 }
