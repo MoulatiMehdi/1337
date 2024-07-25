eb55717d10a366bd2631895354a55828507e635c
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-07-25 01:47:16 +0100

    move macros and library to ft_util.h -> ft.h

--- in_progress/BSQ/includes/ft.h
+++ in_progress/BSQ/includes/ft.h
@@ -13,6 +13,9 @@
 #ifndef FT_H
 #define FT_H
 
+#define BUFF_SIZE 1024
+
+#include <fcntl.h>
 #include "ft_utils.h"
 
 typedef struct s_grid
@@ -25,18 +28,25 @@ typedef struct s_grid
 	char ostacle;
 } t_grid;
 
-
 // allocate
 int	**create_matrix(int rows, int cols);
 void	init_matrix(int **matrx, struct s_grid map);
-void free_grid(void ***arr_2d);
-struct s_grid create_grid(char *str);
-// solve 
-void	find_max_square(struct s_grid map);
+void	free_grid(void ***arr_2d);
+char	**create_strs(int rows, int cols);
+
+// map.c
+void print_grid(struct s_grid map);
+struct	s_grid create_grid(char *str);
+
+// solve
+void solve(char *str);
 
 // parse & validate
 int is_valid_data(char **data);
 char *get_sets(char *str);
 int get_height(char *str);
 
+// read
+char *read_input();
+char *read_file(char *filename);
 #endif
--- in_progress/BSQ/includes/ft_utils.h
+++ in_progress/BSQ/includes/ft_utils.h
@@ -1,11 +1,9 @@
 #ifndef FT_UTILS_H
 #define FT_UTILS_H
 
-#define BUFF_SIZE 1024
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <fcntl.h>
 
 void *ft_realloc(void *mem, int size);
 void	ft_putstr(char *str);
