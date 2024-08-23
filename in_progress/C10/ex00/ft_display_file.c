83ae07fb296328278de95793db6e73b89dee653b
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-08-23 14:22:21 +0100

    improve code of the C10

--- in_progress/C10/ex00/ft_display_file.c
+++ in_progress/C10/ex00/ft_display_file.c
@@ -1,43 +1,27 @@
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-int read_file(char *filename)
-{
+int read_file(char *filename) {
 	int fd;
-	int bytes;
-	unsigned char buffer[1024];
+	char c;
 
-	fd = open(filename,O_RDONLY);
-	if(fd < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return -1;
 
-	bytes = 1 ; 
-	while(bytes > 0)
-	{
-		bytes = read(fd,buffer,1024);
-		write(1,buffer,bytes);
-	}
+	while (read(fd, &c, 1) > 0)
+		write(1, &c, 1);
+
 	close(fd);
 	return 0;
 }
 
-int main(int argc,char **argv)
-{
-	if(argc == 1)
-	{
-		write(2,"File name missing.\n",19);
-		return 1;
-	}
-	else if(argc > 2)
-	{
-		write(2,"Too many arguments.\n",20);
-		return 1;
-	}
-	
-	if(read_file(argv[1]) < 0)
-	{
-		write(2,"Cannot read file.\n",18);
-		return 1;
-	}
+int main(int argc, char **argv) {
+	if (argc == 1)
+		write(2, "File name missing.\n", 19);
+	else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else if (read_file(argv[1]) < 0)
+		write(2, "Cannot read file.\n", 18);
 	return 0;
 }
