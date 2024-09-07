b734fdb7b21c48db95bde648a31ba6868a0bf74c
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-07 17:00:15 +0100

    add C12 files (empty)

---
+++ in_progress/C12/ex00/ft_create_elem.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoulati <mmoulati@1337.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 21:28:54 by mmoulati          #+#    #+#             */
+/*   Updated: 2024/09/07 16:18:38 by mmoulati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*new_elem;
+
+	new_elem = malloc(sizeof(t_list));
+	if (!new_elem)
+		return (0);
+	new_elem->data = data;
+	new_elem = 0;
+	return (new_elem);
+}
