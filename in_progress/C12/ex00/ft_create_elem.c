3fdeb550ccc4bc0f5958b6f5039c3864ec8608c2
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-08 20:45:33 +0100

    fix bug : function not returning the new item

--- in_progress/C12/ex00/ft_create_elem.c
+++ in_progress/C12/ex00/ft_create_elem.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@1337.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 21:28:54 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/09/07 19:26:22 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/09/08 20:11:16 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ t_list	*ft_create_elem(void *data)
 	if (!new_elem)
 		return (0);
 	new_elem->data = data;
-	new_elem = 0;
+	new_elem->next = 0;
 	return (new_elem);
 }
