94c72d68d3457632f6935f7f0aaebb8d5ee02f88
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-08 02:15:16 +0100

    finish the ex16-ex17 of C12 (not tested)

--- in_progress/C12/ex16/ft_sorted_list_insert.c
+++ in_progress/C12/ex16/ft_sorted_list_insert.c
@@ -6,8 +6,36 @@
 /*   By: mmoulati <mmoulati@1337.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 19:44:24 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/09/07 19:44:24 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/09/08 00:58:33 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+
+void	ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
+{
+	t_list	*new_elem;
+	t_list	*prev;
+	t_list	*curr;
+
+	new_elem = ft_create_elem(data);
+	if (!new_elem)
+		return ;
+	if (!begin_list || !*begin_list)
+	{
+		*begin_list = new_elem;
+		return ;
+	}
+	prev = 0;
+	curr = *begin_list;
+	while (curr && (*cmp)(curr->data, data) < 0)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+	if (!prev)
+		*begin_list = new_elem;
+	else
+		prev->next = new_elem;
+	new_elem->next = curr;
+}
--- in_progress/C12/ex17/ft_sorted_list_merge.c
+++ in_progress/C12/ex17/ft_sorted_list_merge.c
@@ -11,3 +11,47 @@
 /* ************************************************************************** */
 
 #include "ft_list.h"
+
+void	ft_list_skip(t_list **prev, t_list *curr, t_list **begin_list2,
+		int (*cmp)())
+{
+	t_list	*tmp;
+
+	while ((*begin_list2)->next && (*cmp)((*begin_list2)->next->data,
+		curr->data) <= 0)
+		*begin_list2 = (*begin_list2)->next;
+	*prev = *begin_list2;
+	tmp = (*begin_list2)->next;
+	(*begin_list2)->next = curr;
+	*begin_list2 = tmp;
+}
+
+void	ft_sorted_list_merge(t_list **begin_list1, t_list *begin_list2,
+		int (*cmp)())
+{
+	t_list	*curr;
+	t_list	*prev;
+
+	if (!begin_list2)
+		return ;
+	if (!begin_list1 || !*begin_list1)
+	{
+		*begin_list1 = begin_list2;
+		return ;
+	}
+	curr = *begin_list1;
+	prev = 0;
+	while (begin_list2)
+	{
+		while (curr && (*cmp)(curr->data, begin_list2->data) < 0)
+		{
+			prev = curr;
+			curr = curr->next;
+		}
+		if (!prev)
+			prev->next = begin_list2;
+		else
+			*begin_list1 = begin_list2;
+		ft_list_skip(&prev, curr, &begin_list2, cmp);
+	}
+}
