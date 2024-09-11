d05e3d1b62f747f67917aa7377249a359c3a9aec
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-11 01:04:24 +0100

    fix list size counter bug

--- in_progress/C12/ex02/ft_list_size.c
+++ in_progress/C12/ex02/ft_list_size.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@1337.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 19:39:54 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/09/07 19:40:17 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/09/11 00:36:21 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	ft_list_size(t_list *begin_list)
 
 	size = 0;
 	while (begin_list)
+	{
+		size++;
 		begin_list = begin_list->next;
+	}
 	return (size);
 }
