0a80f5aa09bfcfd00ed95555b7b8a556e7c9563a
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-11 01:02:53 +0100

    add tests

--- in_progress/C12/tests/srcs/main.c
+++ in_progress/C12/tests/srcs/main.c
@@ -6,18 +6,22 @@
 /*   By: mehdi <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:26:46 by mehdi             #+#    #+#             */
-/*   Updated: 2024/09/08 19:08:30 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/09/10 22:51:11 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	test_push_front(void);
 void	test_create_elem(void);
 void	test_push_back(void);
+void	test_list_size(void);
+void	test_list_last(void);
 
 int	main(void)
 {
 	test_create_elem();
 	test_push_front();
+	test_list_size();
+	test_list_last();
 	test_push_back();
 	return (0);
 }
