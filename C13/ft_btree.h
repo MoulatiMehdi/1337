2b540fb850418ffea5ee78b3144134702aa6fc48
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-13 20:38:14 +0100

    add Day C13 (empty)

---
+++ C13/ft_btree.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoulati <mmoulati@1337.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 20:03:44 by mmoulati          #+#    #+#             */
+/*   Updated: 2024/09/13 20:04:35 by mmoulati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}					t_btree;
+
+t_btree				*btree_create_node(void *item);
