1625d5cea9020d8e07e5271d7b7236cf31d424a9
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-16 22:30:36 +0100

    fix bug in ex05 : if statemnt should be strcmp == 0

--- C13/ex05/btree_search_item.c
+++ C13/ex05/btree_search_item.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@1337.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 22:02:54 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/09/13 22:21:39 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/09/16 20:47:34 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*btree_search_item(t_btree *root, void *data_ref, int (*cmpf)(void *,
 	target = btree_search_item(root->left, data_ref, cmpf);
 	if (target)
 		return (target);
-	if (cmpf(data_ref, root->item))
+	if (cmpf(data_ref, root->item) == 0)
 		return (root->item);
 	return (btree_search_item(root->right, data_ref, cmpf));
 }
